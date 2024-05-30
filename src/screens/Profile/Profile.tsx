e7ad0ed608cadf60feb90e0a6d6a283c2f1d50c4
Lybio Croton de Moraes Junior <lybiomoraesjr@gmail.com>
2024-05-30 15:38:05 -0300

    add profile

---
+++ src/screens/Profile/Profile.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Container } from "./Profile.styles";
+import { Text } from "react-native";
+
+const Profile: React.FC = () => {
+  return (
+    <Container>
+      <Text>Profile</Text>
+    </Container>
+  );
+};
+
+export default Profile;
