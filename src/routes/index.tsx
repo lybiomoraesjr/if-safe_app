565d369fab820d056de0bda12a57c2705a26e9d2
Lybio Croton de Moraes Junior <lybiomoraesjr@gmail.com>
2024-05-30 20:06:19 -0300

    add controllers

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,12 @@
 import { NavigationContainer } from "@react-navigation/native";
 
 import { AppRoutes } from "./app.routes";
+import { AuthRoutes } from "./auth.routes";
 
 export function Routes() {
   return (
     <NavigationContainer>
-      <AppRoutes />
+      <AuthRoutes />
     </NavigationContainer>
   );
 }
--- src/screens/SignUp/SignUp.tsx
+++ src/screens/SignUp/SignUp.tsx
@@ -5,15 +5,36 @@ import { Image } from "react-native";
 import { Button, Input } from "@rneui/base";
 import { useNavigation } from "@react-navigation/native";
 import { AuthNavigatorRoutesProps } from "../../routes/auth.routes";
+import { Controller, useForm } from "react-hook-form";
+
+type FormDataProps = {
+  name: string;
+  email: string;
+  password: string;
+  password_confirm: string;
+};
 
 const SignUn: React.FC = () => {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormDataProps>();
+
   const navigation = useNavigation<AuthNavigatorRoutesProps>();
 
   const handleGoBack = () => {
-    navigation.navigate("signIn");
+    navigation.goBack();
   };
 
-  const handleSignUp = () => {};
+  const handleSignUp = ({
+    name,
+    email,
+    password,
+    password_confirm,
+  }: FormDataProps) => {
+    console.log({ name, email, password, password_confirm });
+  };
 
   return (
     <Container>
@@ -21,13 +42,67 @@ const SignUn: React.FC = () => {
         <Image source={require("./../../assets/ifsafe-logo.png")} />
       </View>
 
-      <Input placeholder="Nome" />
+      <Controller
+        control={control}
+        name="name"
+        rules={{ required: "Campo obrigatório" }}
+        render={({ field: { onChange, value } }) => (
+          <Input placeholder="Nome" onChangeText={onChange} value={value} />
+        )}
+      />
 
-      <Input placeholder="E-mail" />
+      <Text>{errors.name?.message}</Text>
 
-      <Input placeholder="Senha" />
+      <Controller
+        control={control}
+        name="email"
+        rules={{
+          required: "Campo obrigatório",
+          pattern: {
+            value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+            message: "E-mail inválido",
+          },
+        }}
+        render={({ field: { onChange, value } }) => (
+          <Input placeholder="E-mail" onChangeText={onChange} value={value} />
+        )}
+      />
 
-      <Input placeholder="Confirme a Senha" />
+      <Text>{errors.email?.message}</Text>
+
+      <Controller
+        control={control}
+        name="password"
+        rules={{ required: "Campo obrigatório" }}
+        render={({ field: { onChange, value } }) => (
+          <Input
+            placeholder="Senha"
+            onChangeText={onChange}
+            value={value}
+            secureTextEntry
+          />
+        )}
+      />
+
+      <Text>{errors.password?.message}</Text>
+
+      <Controller
+        control={control}
+        name="password_confirm"
+        rules={{ required: "Campo obrigatório" }}
+        render={({ field: { onChange, value } }) => (
+          <Input
+            placeholder="Confirme a Senha"
+            onChangeText={onChange}
+            value={value}
+            secureTextEntry
+            onSubmitEditing={handleSubmit(handleSignUp)}
+            returnKeyType="send"
+          />
+        )}
+      />
+
+      <Text>{errors.password_confirm?.message}</Text>
 
       <Button
         title="Acessar"
@@ -44,7 +119,7 @@ const SignUn: React.FC = () => {
           width: 200,
           marginVertical: 10,
         }}
-        onPress={() => console.log("aye")}
+        onPress={handleSubmit(handleSignUp)}
       />
 
       <Button
