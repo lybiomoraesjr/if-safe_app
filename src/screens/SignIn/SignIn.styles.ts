d3848e2af8e74bf72a3dd91c1884e598f891e267
Lybio Croton de Moraes Junior <lybiomoraesjr@gmail.com>
2024-05-30 18:14:20 -0300

    add inputs and navigation to signin

--- src/screens/SignIn/SignIn.styles.ts
+++ src/screens/SignIn/SignIn.styles.ts
@@ -2,4 +2,6 @@ import styled from "styled-components/native";
 
 export const Container = styled.ImageBackground`
   flex: 1;
+
+  background-color: ${({ theme }) => theme.COLORS.WHITE};
 `;
--- src/screens/SignIn/SignIn.tsx
+++ src/screens/SignIn/SignIn.tsx
@@ -1,11 +1,64 @@
 import React from "react";
 import { Container } from "./SignIn.styles";
 import { Input } from "@rneui/base";
+import { Image, Text, View } from "react-native";
+import { Button } from "@rneui/themed";
+import { useNavigation } from "@react-navigation/native";
+import { AuthNavigatorRoutesProps } from "../../routes/auth.routes";
 
 const SignIn: React.FC = () => {
+  const navigation = useNavigation<AuthNavigatorRoutesProps>();
+
+  const handleNewAccount = () => {
+    navigation.navigate("signUp");
+  };
+
   return (
     <Container>
-      <Input placeholder="BASIC INPUT" />
+      <View>
+        <Image source={require("./../../assets/ifsafe-logo.png")} />
+      </View>
+      <Input placeholder="E-mail" />
+
+      <Input placeholder="Senha" />
+
+      <Button
+        title="Acessar"
+        loading={false}
+        loadingProps={{ size: "small", color: "white" }}
+        buttonStyle={{
+          backgroundColor: "rgba(111, 202, 186, 1)",
+          borderRadius: 5,
+        }}
+        titleStyle={{ fontWeight: "bold", fontSize: 23 }}
+        containerStyle={{
+          marginHorizontal: 50,
+          height: 50,
+          width: 200,
+          marginVertical: 10,
+        }}
+        onPress={() => console.log("aye")}
+      />
+
+      <Text>Ainda não tem acesso?</Text>
+
+      <Button
+        title="Criar Conta"
+        loading={false}
+        loadingProps={{ size: "small", color: "white" }}
+        buttonStyle={{
+          backgroundColor: "rgba(111, 202, 186, 1)",
+          borderRadius: 5,
+        }}
+        titleStyle={{ fontWeight: "bold", fontSize: 23 }}
+        containerStyle={{
+          marginHorizontal: 50,
+          height: 50,
+          width: 200,
+          marginVertical: 10,
+        }}
+        onPress={handleNewAccount}
+      />
     </Container>
   );
 };
