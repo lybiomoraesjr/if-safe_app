ae5a60775df4e8d56aec7d0a4672987462d6ddbc
Lybio Croton de Moraes Junior <lybiomoraesjr@gmail.com>
2024-05-30 18:10:43 -0300

    add input

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Container } from "./Input.styles";
+import { Text } from "react-native";
+
+const Input: React.FC = () => {
+  return (
+    <Container>
+      <Text>Input</Text>
+    </Container>
+  );
+};
+
+export default Input;
