08b13f5f313c1417d45be070ee76e7df580a93b9
Lybio Croton de Moraes Junior <lybiomoraesjr@gmail.com>
2024-05-30 15:12:33 -0300

    add ocurrencedetailmock

---
+++ src/utils/ocurrenceDetailMock.ts
@@ -0,0 +1,16 @@
+import { OccurrenceDetails, OccurrenceStatus } from "../types/types";
+
+export const ocurrenceDetailMock: OccurrenceDetails = {
+  uuid: "occurrenceId1",
+  title: "Fio desencapado",
+  imageUri:
+    "https://i0.wp.com/www.horalivre.com.br/wp-content/uploads/2021/02/phb-para-4.jpg?fit=600%2C450&ssl=1",
+  description: "descrição 1",
+  notifiersIDs: ["userId2", "userId3"],
+  status: OccurrenceStatus.UNSOLVED,
+  author: {
+    uuid: "userId1",
+    name: "Lybio Moraes Junior",
+  },
+  date: new Date(),
+};
