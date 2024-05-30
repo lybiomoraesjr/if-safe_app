913fbef76d17ea206479d5f1fb1580f46e9b4e2e
Lybio Croton de Moraes Junior <lybiomoraesjr@gmail.com>
2024-05-30 12:59:41 -0300

    customize status bar

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar
+          barStyle="dark-content"
+          backgroundColor="transparent"
+          translucent
+        />
     </View>
   );
 }
