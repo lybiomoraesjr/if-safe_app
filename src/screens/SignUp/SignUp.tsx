f23275e5aafac599b19951c784262415092b4639
Lybio Croton de Moraes Junior <lybiomoraesjr@gmail.com>
2024-05-30 15:13:05 -0300

    add signun

---
+++ src/screens/SignUp/SignUp.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Container } from "./SignUp.styles";
+import { Text } from "react-native";
+
+const SignUn: React.FC = () => {
+  return (
+    <Container>
+      <Text>Olá, Mundo!</Text>
+    </Container>
+  );
+};
+
+export default SignUn;
