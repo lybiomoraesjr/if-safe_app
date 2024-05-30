7fa9c86692663710d21b53cc030a4bc11d267d5f
Lybio Croton de Moraes Junior <lybiomoraesjr@gmail.com>
2024-05-30 18:10:32 -0300

    add button

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text } from "react-native";
+import { Container } from "./Button.styles";
+
+const Button: React.FC = () => {
+  return (
+    <Container>
+      <Text>Button</Text>
+    </Container>
+  );
+};
+
+export default Button;
