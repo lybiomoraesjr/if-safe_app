eaa34745c6ae8274ba7a559e188e123fe32091bc
ThalesMiranda <thalesmiranda.contato@gmail.com>
2024-05-30 21:14:48 -0300

    Button Component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,13 +1,32 @@
 import React from "react";
 import { Text } from "react-native";
 import { Container } from "./Button.styles";
+import { Button } from "@rneui/base";
 
-const Button: React.FC = () => {
+type ButtonComponentProps = {
+  isLoading?: boolean;
+};
+
+const ButtonComponent: React.FC<ButtonComponentProps> = ({
+  isLoading = false,
+}) => {
   return (
-    <Container>
-      <Text>Button</Text>
-    </Container>
+    <Button
+      loading={isLoading}
+      loadingProps={{ size: "small", color: "white" }}
+      buttonStyle={{
+        backgroundColor: "rgba(111, 202, 186, 1)",
+        borderRadius: 5,
+      }}
+      titleStyle={{ fontWeight: "bold", fontSize: 23 }}
+      containerStyle={{
+        marginHorizontal: 50,
+        height: 50,
+        width: 200,
+        marginVertical: 10,
+      }}
+    />
   );
 };
 
-export default Button;
+export default ButtonComponent;
