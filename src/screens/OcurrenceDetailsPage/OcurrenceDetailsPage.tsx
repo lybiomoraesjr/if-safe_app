092c569a46381bdb88d7f57d61088421e7157487
Lybio Croton de Moraes Junior <lybiomoraesjr@gmail.com>
2024-05-31 10:38:54 -0300

    add OcurrenceDetailsPage

---
+++ src/screens/OcurrenceDetailsPage/OcurrenceDetailsPage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const OcurrenceDetailsPage: React.FC = () => {
+  return (
+    <div>
+      <h1>OcurrenceDetails</h1>
+    </div>
+  );
+};
+
+export default OcurrenceDetailsPage;
