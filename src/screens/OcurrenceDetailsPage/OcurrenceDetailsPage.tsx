5e470306c654276189936d47a17d394025068c13
Lybio Croton de Moraes Junior <lybiomoraesjr@gmail.com>
2024-05-31 16:54:36 -0300

    add HomeHeader to OcurrenceDetailsPage

--- src/screens/OcurrenceDetailsPage/OcurrenceDetailsPage.tsx
+++ src/screens/OcurrenceDetailsPage/OcurrenceDetailsPage.tsx
@@ -2,10 +2,14 @@ import React from "react";
 import { View } from "react-native";
 import OcurrenceDetails from "../../components/OcurrenceDetails";
 import { ocurrenceDetailsMock } from "../../utils/ocurrenceDetailsMock";
+import { userMock } from "../../utils/userMock";
+import HomeHeader from "../../components/HomeHeader";
 
 const OcurrenceDetailsPage: React.FC = () => {
   return (
     <View>
+      <HomeHeader user={userMock} />
+
       <OcurrenceDetails ocurrenceDetails={ocurrenceDetailsMock} />
     </View>
   );
