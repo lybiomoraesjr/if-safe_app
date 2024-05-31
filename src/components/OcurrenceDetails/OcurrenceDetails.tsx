cd6003dc3c4e30a73cb4d5706882c530857a5095
Lybio Croton de Moraes Junior <lybiomoraesjr@gmail.com>
2024-05-31 10:41:56 -0300

    add OcurrenceDetails Component

---
+++ src/components/OcurrenceDetails/OcurrenceDetails.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type OcurrenceDetailsProps = {};
+
+const OcurrenceDetails: React.FC<OcurrenceDetailsProps> = () => {
+  return (
+    <div>
+      <h1>Ocurrence Details</h1>
+    </div>
+  );
+};
+
+export default OcurrenceDetails;
