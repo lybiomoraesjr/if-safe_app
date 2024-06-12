99f2c4cd95bfc93b4ff62e5b4e3a7618cd710e3c
Lybio Moraes Junior <lybiomoraesjr@gmail.com>
2024-06-12 11:38:37 -0300

    fix text messaging bug

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,4 @@
 import axios from "axios";
-import { AppError } from "../utils/AppError";
 
 const api = axios.create({
   baseURL: "http://ifsafe-ifsp.vercel.app",
@@ -9,7 +8,7 @@ api.interceptors.response.use(
   (response) => response,
   (error) => {
     if (error.response && error.response.data) {
-      return Promise.reject(new AppError(error.response.data.message));
+      return Promise.reject(error.response.data);
     } else {
       return Promise.reject(error);
     }
