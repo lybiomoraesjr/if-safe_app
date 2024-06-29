27e489ee0acb36d32d30c6409cc9dd052d33a324
Lybio Moraes Junior <lybiomoraesjr@gmail.com>
2024-06-28 23:56:19 -0300

    refrash

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -3,7 +3,7 @@ import { Container, Title } from "./Home.styles";
 import HomeHeader from "../../components/HomeHeader";
 import { useNavigation } from "@react-navigation/native";
 import { AppNavigatorRoutesProps } from "../../routes/app.routes";
-import { Alert, FlatList, Text, View } from "react-native";
+import { Alert, FlatList, RefreshControl, Text, View } from "react-native";
 import { OccurrenceStatusEnum } from "@/types";
 import Status from "../../components/Status";
 import OccurrenceCard from "@/components/OccurrenceCard";
@@ -11,12 +11,15 @@ import Loading from "@/components/Loading";
 import { useAuth } from "@/hooks/useAuth";
 import { useOccurrence } from "@/hooks/useOccurrence";
 import { AppError } from "@/utils/AppError";
+import { useTheme } from "styled-components";
 
 const Home: React.FC = () => {
   const [activeFilter, setActiveFilter] = useState<string>("all");
   const [isLoading, setIsLoading] = useState(true);
+  const [refreshing, setRefreshing] = useState(false);
   const { navigate } = useNavigation<AppNavigatorRoutesProps>();
 
+  const { COLORS } = useTheme();
   const { user } = useAuth();
 
   const { fetchOccurrenceCards, occurrenceCards } = useOccurrence();
@@ -35,72 +38,96 @@ const Home: React.FC = () => {
 
   const occurrenceKeys = Object.keys(OccurrenceFilter);
 
+  const fetchData = async () => {
+    try {
+      setIsLoading(true);
+      await fetchOccurrenceCards();
+    } catch (error) {
+      const isAppError = error instanceof AppError;
+      const title = isAppError
+        ? error.data
+        : "Não foi possível carregar as ocorrências";
+      Alert.alert("Erro", title);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        setIsLoading(true);
-        await fetchOccurrenceCards();
-      } catch (error) {
-        const isAppError = error instanceof AppError;
-        const title = isAppError
-          ? error.data
-          : "Não foi possível carregar as ocorrências";
-
-        setIsLoading(false);
-        Alert.alert("Erro", title);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
     fetchData();
   }, []);
+
+  const handleRefresh = async () => {
+    try {
+      setRefreshing(true);
+      await fetchOccurrenceCards();
+    } catch (error) {
+      const isAppError = error instanceof AppError;
+      const title = isAppError
+        ? error.data
+        : "Não foi possível carregar as ocorrências";
+      Alert.alert("Erro", title);
+    } finally {
+      setRefreshing(false);
+    }
+  };
 
   return (
     <Container>
       <HomeHeader />
 
-      <FlatList
-        data={occurrenceKeys.filter(
-          (item) => user.admin || item !== "cancelled"
-        )}
-        keyExtractor={(item) => item}
-        renderItem={({ item }) => (
-          <Status
-            name={OccurrenceFilter[item]}
-            variant={activeFilter === item ? "active" : "inactive"}
-            onPress={() => setActiveFilter(item)}
-          />
-        )}
-        horizontal
-        showsHorizontalScrollIndicator={false}
-      />
-
-      <Title>Ocorrências:</Title>
-
-      {isLoading ? (
-        <Loading />
-      ) : (
+      <View>
         <FlatList
-          data={occurrenceCards}
+          data={occurrenceKeys.filter(
+            (item) => user.admin || item !== "cancelled"
+          )}
+          keyExtractor={(item) => item}
           renderItem={({ item }) => (
-            <OccurrenceCard
-              image={item.image}
-              alert={item.likes}
-              status={item.status}
-              title={item.title}
-              date={item.date}
-              onInteract={() => handleNavigateToOccurrence(item._id)}
+            <Status
+              name={OccurrenceFilter[item]}
+              variant={activeFilter === item ? "active" : "inactive"}
+              onPress={() => setActiveFilter(item)}
             />
           )}
-          ListEmptyComponent={() => (
-            <View>
-              <Text>Não há ocorrências disponíveis no momento.</Text>
-            </View>
-          )}
-          showsVerticalScrollIndicator={false}
+          horizontal
+          showsHorizontalScrollIndicator={false}
         />
-      )}
+
+        <Title>Ocorrências:</Title>
+
+        {isLoading ? (
+          <Loading />
+        ) : (
+          <FlatList
+            data={occurrenceCards}
+            renderItem={({ item }) => (
+              <OccurrenceCard
+                image={item.image}
+                alert={item.likes}
+                status={item.status}
+                title={item.title}
+                date={item.date}
+                onInteract={() => handleNavigateToOccurrence(item._id)}
+              />
+            )}
+            ListEmptyComponent={() => (
+              <View>
+                <Text>Não há ocorrências disponíveis no momento.</Text>
+              </View>
+            )}
+            showsVerticalScrollIndicator={false}
+            refreshing={refreshing}
+            onRefresh={handleRefresh}
+            refreshControl={
+              <RefreshControl
+                refreshing={refreshing}
+                onRefresh={handleRefresh}
+                colors={[COLORS.BRAND_LIGHT]}
+              />
+            }
+          />
+        )}
+      </View>
     </Container>
   );
 };
